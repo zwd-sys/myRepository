05149e8361daa6c8f094e0d8d0e56d4bbff73d41
zhao2794499249 <2794499249@qq.com>
2024-07-10 11:52:14 +0800

    Add files via upload

---
+++ Good.java
@@ -0,0 +1,16 @@
+package com.cdtu.model;
+
+import lombok.Data;
+
+@Data
+public class Good {
+    private int gid;
+    private String name;
+    private String img;
+    private float price;
+    private int num;
+	//private int num;
+	//private int num;
+	//private int num;
+	
+}
