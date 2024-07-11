1194c23f2220e46c5ef2c09ea9293cdba9916c37
lovekwc <3442871288@qq.com>
2024-07-11 10:29:06 +0800

    修改后提交

--- src/main/java/com/cdtu/test/Testt.java
+++ src/main/java/com/cdtu/test/Testt.java
@@ -3,6 +3,10 @@ package com.cdtu.test;
 import com.cdtu.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class Testt {
+import java.sql.SQLOutput;
 
+public class Testt {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
 }
